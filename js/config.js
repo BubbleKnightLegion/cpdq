bdcfcc956722abb7547a96d2aed576ca9aee3170
wang yang <1002289593@qq.com>
2018-07-20 19:07:03 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,26 @@
+var config = {
+  
+  // 菜谱大全
+  query: "http://apis.juhe.cn/cook/query.php",
+  category: "http://apis.juhe.cn/cook/category?key=",
+  AppKey: "36aeaf585dfb772e6ce2d751182dc6f7",
+  index: "http://apis.juhe.cn/cook/index",
+  SetTheme: function() {
+    if (localStorage.getItem("theme") == null) {
+    } else {
+      $("body").addClass("theme-dark");
+    }
+
+    $(".model-change").on("click", function() {
+      if (localStorage.getItem("theme") == "true") {
+        $("body").removeClass("theme-dark");
+        localStorage.removeItem("theme");
+        $.toast("开启白天模式");
+      } else {
+        $("body").addClass("theme-dark");
+        localStorage.setItem("theme", true);
+        $.toast("开启黑夜模式");
+      }
+    });
+  }
+};
